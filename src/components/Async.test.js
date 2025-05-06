c4d38ed014a8a808707f5e696c966298520bef3f
andxbes <andxbes@gmail.com>
2025-05-06 21:41:46 +0300

    576

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,15 @@ import Async from "./Async";
 
 describe('Async component', () => {
     test('render posts if request success', async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [
+                {
+                    id: 'p1',
+                    title: 'First post'
+                }
+            ]
+        });
         render(<Async />);
         const listItemElements = await screen.findAllByRole('listitem', {}, {});
         expect(listItemElements).not.toHaveLength(0);
